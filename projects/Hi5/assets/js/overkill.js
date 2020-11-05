aa6d8ddbd42df7181b7e81299cb40fc6902f9ecf
gspteck <66552124+gspteck@users.noreply.github.com>
2020-11-05 22:06:09 +0100

    random edits

---
+++ projects/Hi5/assets/js/overkill.js
@@ -0,0 +1,49 @@
+const tl = gsap.timeline({defaults:{ease: 'power1.out'}});
+
+tl.to('.text', {y:"0%", duration: 1, stagger: 0.25});
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+
+//making draggable logo
+dragElement(document.getElementById("logo"));
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+function load_vid() {    
+    setTimeout(() => {
+        var channelID = "UCluqnG4yYHGsfRuV-_wjHNw";
+        var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+        $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
+        var link = data.items[0].link;
+        var id = link.substr(link.indexOf("=")+1);
+        $("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
+        });
+    }, 5000);
+}
