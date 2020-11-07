c17f3a876c7c84bb9ff6fca1f69f5e95e4560031
gspteck <66552124+gspteck@users.noreply.github.com>
2020-11-07 08:45:35 +0100

    random edits

---
+++ projects/hi5studios.com/assets/js/overkill.js
@@ -0,0 +1,29 @@
+function load_vid() {    
+    setTimeout(() => {
+        var channelID = "UCluqnG4yYHGsfRuV-_wjHNw";
+        var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+        $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
+        var link = data.items[0].link;
+        var id = link.substr(link.indexOf("=")+1);
+        $("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
+        });
+    }, 5000);
+}
+
+function remv_login() {
+    var block = document.getElementById('block');
+    block.innerHTML = ' ';
+    load_vid();
+}
+
+function mega_bg() {
+    document.body.style.background = "url('../assets/images/wallpaper.jpg')";
+    var megabg = document.getElementById('mega-bg');
+    megabg.outerHTML = '<button id="mega-bg" onclick="bl_bg()">normal bg</button>';
+}
+
+function bl_bg() {
+    document.body.style.background = "black";
+    var megabg = document.getElementById('mega-bg');
+    megabg.outerHTML = '<button id="mega-bg" onclick="mega_bg()">MEGA BG</button>';
+}
